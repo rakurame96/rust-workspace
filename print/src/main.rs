a71195f3a5f6403757ee568a0b7de48211419264
Rakuram <rakuram.e96@gmail.com>
2024-03-16 01:07:10 +0530

    initial commit

---
+++ print/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, Rust World!");  // Prints with a newline
+    print!("This stays on the same line "); // No newline
+    println!("...and now we're on a new line.");
+
+        println!{"Hi"};
+}
