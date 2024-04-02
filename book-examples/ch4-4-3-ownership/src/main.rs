325ede299bb399dcb056fa0eece9aaa9c822e52b
Rakuram <rakuram.e96@gmail.com>
2024-04-02 22:12:26 +0530

    added comments

--- book-examples/ch4-4-3-ownership/src/main.rs
+++ book-examples/ch4-4-3-ownership/src/main.rs
@@ -1,22 +1,27 @@
 fn main() {
-    let s = String::from("hello");
+    let s = String::from("hello");      // s comes into the scope
 
-    takes_ownership(s);
+    takes_ownership(s);             // s's value moves into the function...
+                                                // ... and so no longer valid here
     // takes_ownership(s.clone());
 
     // println!("{}", s);      // throws error as s's value moves into the function
                             // and so no longer valid here
     // to make the above line work, call takes_ownership(s.clone())
     
-    let x = 5;
+    let x = 5;                              // x comes into scope
 
-    makes_copy(x);
-}
+    makes_copy(x);                  // x would move into the function 
+                                                 // but i32 is a copy, so it is okay to still
+                                                 // use x afterward
+}   // here x goes out of scope, then s. However, because s's value was moved,
+    // nothing special happens here
 
-fn takes_ownership(some_string: String) {
+fn takes_ownership(some_string: String) {       // some_string comes into the scope
     println!("{some_string}");
-}
+}   // here, some_string goes out of the scope and 'drop' is called. the backing
+    // memory is freed
 
-fn makes_copy(some_integer:i32) {
+fn makes_copy(some_integer:i32) {               // some_integer comes into the scope
     println!("{some_integer}");
-}
+}   // here, some_integer goes out of the scope. nothing special happens here
--- book-examples/ch4-4-4-return-values/src/main.rs
+++ book-examples/ch4-4-4-return-values/src/main.rs
@@ -1,22 +1,27 @@
 fn main() {
-    let s1 = gives_overship();
+    let s1 = gives_overship();      // gives_ownership moves its return value into s1
     println!("s1 : {}", s1);
 
-    let s2 = String::from("hello");
+    let s2 = String::from("hello"); // s2 comes into scope
 
-    let s3 = takes_and_gives_back(s2);
+    let s3 = takes_and_gives_back(s2);  // s2 moved into takes_and_gives_back, which also move its
+                                                         // return value into s3
     println!("s3 : {}", s3);
+}   // s3 goes out of scope and is dropped. s2 was moved, so nothing happens
+    // s1 goes out of scope and is dropped
+
+fn gives_overship() -> String {             // gives_overship will move its return value into the function that calls it.
+    let some_string = String::from("yours");    // some_string comes into scope
+
+     // some_string         // return value to the caller
+    return some_string;     // some_string is returned and
+                            // moves out to the calling
+                            // function
 }
 
-fn gives_overship() -> String {
-    let some_string = String::from("yours");
-
-    // some_string         // return value to the caller
-    return some_string;
-}
-
-fn takes_and_gives_back(mut a_string: String) -> String {
+// This function takes a String and returns a String.
+fn takes_and_gives_back(mut a_string: String) -> String {   // a_string comes into scope with mutability
     a_string.push_str(" rustaceans!!!");
     
-    a_string
+    a_string    // a_string is returned and moves out to the calling function with additional character attached to it because of mutability
 }
