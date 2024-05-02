13080156513fabf977a4232d9e7cc1fbb42b3f7f
Rakuram <rakuram.e96@gmail.com>
2024-05-03 00:04:51 +0530

    chap-7 multiple lib files

---
+++ book-examples/ch7-7-16-new-names-as-kywd/src/lib.rs
@@ -0,0 +1,32 @@
+// use std::fmt;
+// use std::io;
+// fn function1() -> fmt::Result {
+// //  --snip--
+// }
+// fn function2() -> io::Result<()> {
+// //  --snip--
+// }
+
+// use std::fmt::Result;
+// use std::io::Result as IoResult;
+
+// nested paths
+use std::{fmt::Result, io::Result as IoResult};
+
+// use std::io;
+// use std::io::Write;
+
+// shorthanded
+use std::{self, io::Write};
+
+// glob operator
+use std::collections::*;    // brings all the public elements into scope
+
+fn function1() -> Result {
+//  --snip--
+    return ();
+}
+fn function2() -> IoResult<()> {
+//  --snip--
+    return ();
+}
