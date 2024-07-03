219bea1dbce623a117293f06d119233e576b4695
Rakuram <rakuram.e96@gmail.com>
2024-07-03 21:52:30 +0530

    update StrSplit project

--- projects/strsplit/src/lib.rs
+++ projects/strsplit/src/lib.rs
@@ -20,6 +20,14 @@ impl<T> Foo<T> {} // by doing this, it will make the impl block as a generic imp
 // '_ - anonmymous lifetime
 // '_ - tick underscore - means a pattern that matches anything (type inferences for references) or otherwise don't consider this lifetime for the purpose of guessing
 // 'a - tick a - specific lifetime same as 
+
+// str -> [char] type (it only stores the start address and total size)
+// &str -> &[char] it is a pointer to a sequence of characters
+// String -> Vec<char> heap allocated. can shrink and grow dynamically
+//
+// String -> &str (possible) -- (AsRef)
+// &str -> String (very trivial as need to copy the contents to heap in order to store it as a String type) -- (Clone)
+
 impl<'a> StrSplit<'a> {
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
@@ -36,20 +44,28 @@ impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
         // find the delimiter in the string slice
-        if let Some(ref mut remainder /* &mut &'a str */) = self.remainder /* Option<&'a str> */
-        {
-            if let Some(next_delim) = remainder.find(self.delimiter) {
-                let until_delimiter = &remainder[..next_delim];
-                // dereferening
-                *remainder = &remainder[(next_delim + self.delimiter.len())..];
-                Some(until_delimiter)
-            } else {
-                self.remainder.take()
-            }
+        // if let Some(ref mut remainder /* &mut &'a str */) = self.remainder /* Option<&'a str> */
+        let remainder = self.remainder.as_mut()?;
+        if let Some(next_delim) = remainder.find(self.delimiter) {
+            let until_delimiter = &remainder[..next_delim];
+            // dereferening
+            *remainder = &remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
         } else {
-            None
+            self.remainder.take()
         }
     }
+}
+
+fn until_char<'s>(s: &'s str, c: char) -> &'s str {
+    StrSplit::new(s, format!("{}", c))
+        .next()
+        .expect("StrSplit always gives at least one result")
+}
+
+#[test]
+fn until_char_test() {
+    assert_eq!(until_char("hello world", 'o'), "hell");
 }
 
 #[test]
