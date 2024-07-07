26183b40cf0ad44e9c310c6a2e66202eec361bc7
Rakuram <rakuram.e96@gmail.com>
2024-07-07 10:07:48 +0530

    update examples

---
+++ book-examples/ch10-10-19-generic-lifetimes/src/main.rs
@@ -0,0 +1,20 @@
+// function to find the longest of 2 string slices 
+// but it does not compile 
+// error: missing lifetime parameter
+
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    }
+    else {
+        s2
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+}
---
+++ book-examples/ch10-10-23-outofscope-lifetimes/src/main.rs
@@ -0,0 +1,23 @@
+// function to find the longest of 2 string slices 
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    }
+    else {
+        s2
+    }
+}
+
+fn main() {
+    // string1 ==> String format not &str slice
+    let string1 = String::from("long string is strong");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    // error: borrowed values does not live long enough
+    // string2 dropped here while still borrowed
+    println!("The longest string is {result}");
+    //                              --------borrow later used here
+}
