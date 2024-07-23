d97c48a0cdde7f964978ff4b569d28584ed7d4f9
Rakuram <rakuram.e96@gmail.com>
2024-07-23 18:56:17 +0530

    update macro project

--- projects/vec_macro/src/lib.rs
+++ projects/vec_macro/src/lib.rs
@@ -14,6 +14,32 @@ macro_rules! own_assert_eq {
     };
 }
 
+#[macro_export]
+macro_rules! vec_macro {
+    ($( $x:expr ),* ) => {
+        let mut v = Vec::new();
+        $(v.push($x); )*
+        v
+    };
+}
+
+#[macro_export]
+macro_rules! vec_macro_comma {
+    ($( $x:expr ),* ,) => {
+        let mut v = Vec::new();
+        $(v.push($x); )*
+        v
+    };
+}
+
+#[macro_export]
+macro_rules! vec_macro_comma_plus {
+    ($( $x:expr ),+ ,) => {
+        let mut v = Vec::new();
+        vec![$( $x ),*]
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +48,20 @@ mod tests {
     // #[should_panic]
     fn it_works() {
         own_assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn vec_test() {
+        vec_macro!(5, 6);
+    }
+
+    #[test]
+    fn vec_test_with_comma() {
+        vec_macro_comma!(5, 6, );
+    }
+
+    #[test]
+    fn vec_test_with_comma_plus() {
+        vec_macro_comma!(5, 6, 7,);
     }
 }
