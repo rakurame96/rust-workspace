dbcdd1ec42b811865ce836d444d466a275eaa0b2
Rakuram <rakuram.e96@gmail.com>
2024-08-02 19:50:38 +0530

    cli-minigrep project

--- projects/cli-minigrep/src/main.rs
+++ projects/cli-minigrep/src/main.rs
@@ -1,6 +1,18 @@
 use std::env;   // to read the commandline arguments
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    //dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("file {}", file_path);
+
+    let fs_contents = fs::read_to_string(file_path)
+                    .expect("should have been able to read the file");
+    
+    println!("With text: \n{}", fs_contents);
 }
