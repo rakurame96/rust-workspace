0164fd044f20243010593f5c21210f854e8c6fba
Rakuram <rakuram.e96@gmail.com>
2024-08-03 14:30:56 +0530

    update cli-grep project

--- projects/cli-minigrep/src/main.rs
+++ projects/cli-minigrep/src/main.rs
@@ -5,8 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    // let query = &args[1];
+    // let file_path = &args[2];
+
+    let (query, file_path) = parse_config(&args);
+
+    
 
     println!("Searching for {}", query);
     println!("file {}", file_path);
@@ -15,4 +19,11 @@ fn main() {
                     .expect("should have been able to read the file");
     
     println!("With text: \n{}", fs_contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
