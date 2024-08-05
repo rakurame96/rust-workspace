1fdf931e45e75cf65db3e9227e4fa828ea0af3f4
Rakuram <rakuram.e96@gmail.com>
2024-08-05 19:02:47 +0530

    update cli-minigrep project

--- projects/cli-minigrep/src/main.rs
+++ projects/cli-minigrep/src/main.rs
@@ -1,29 +1,57 @@
-use std::env;   // to read the commandline arguments
+use std::env;   use std::error::Error;
+// to read the commandline arguments
 use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+        }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
 
-    // let query = &args[1];
-    // let file_path = &args[2];
-
-    let (query, file_path) = parse_config(&args);
-
+    // let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     
 
-    println!("Searching for {}", query);
-    println!("file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("file {}", config.file_path);
 
-    let fs_contents = fs::read_to_string(file_path)
-                    .expect("should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+#[allow(unused)]
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let fs_contents = fs::read_to_string(config.file_path)?;
     
-    println!("With text: \n{}", fs_contents);
+    Ok(())
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
 
-    (query, file_path)
-}
+//     Config { query, file_path }
+// }
