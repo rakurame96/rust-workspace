18edcb171af3db0eda2516525b320cf451dcf12c
Rakuram <rakuram.e96@gmail.com>
2024-09-01 11:50:40 +0530

    refactoring cli-project

---
+++ projects/cli-minigrep/src/lib.rs
@@ -0,0 +1,46 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let fs_contents = fs::read_to_string(config.file_path)?;
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(test)]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        Safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["Safe, fast, productive."], search(query, contents));
+    }
+}
--- projects/cli-minigrep/src/main.rs
+++ projects/cli-minigrep/src/main.rs
@@ -1,26 +1,7 @@
-use std::env;   use std::error::Error;
-// to read the commandline arguments
-use std::fs;
+use std::env;
 use std::process;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-        }
-}
+use cli_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,27 +12,12 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    
 
     println!("Searching for {}", config.query);
     println!("file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = cli_minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 }
-
-#[allow(unused)]
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let fs_contents = fs::read_to_string(config.file_path)?;
-    
-    Ok(())
-}
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let file_path = args[2].clone();
-
-//     Config { query, file_path }
-// }
