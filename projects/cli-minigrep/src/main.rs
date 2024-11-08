e607b5422ff0b959040fb1b177c224822ae7b84a
Rakuram <rakuram.e96@gmail.com>
2024-11-08 23:25:16 +0530

    update cli-minigrep project

--- projects/cli-minigrep/src/main.rs
+++ projects/cli-minigrep/src/main.rs
@@ -8,10 +8,17 @@ fn main() {
     //dbg!(args);
 
     // let config = Config::new(&args);
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
+    
+    // using iterators idea
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+
 
     // println!("Searching for {}", config.query);
     // println!("file {}", config.file_path);
