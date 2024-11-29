85be9643d747e2cd8f63ee1bfe0403183f0f254b
Rakuram <rakuram.e96@gmail.com>
2024-11-29 17:21:46 +0530

    update tcp-listener project

--- projects/tcp_listener/src/main.rs
+++ projects/tcp_listener/src/main.rs
@@ -1,9 +1,13 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{prelude::*, BufReader};
-use std::fs;
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 
 fn main() {
-    // The bind function in this scenario works like the new function in that it will return a new TcpListener instance. 
+    // The bind function in this scenario works like the new function in that it will return a new TcpListener instance.
     // The function is called bind because, in networking, connecting to a port to listen to is known as “binding to a port".
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -15,6 +19,50 @@ fn main() {
     }
 }
 
+// Before introducing thread
+// fn handle_connection(mut stream: TcpStream) {
+//     let buf_reader = BufReader::new(&mut stream);
+
+//     /* http request message */
+//     // Method Request-URI HTTP-Version CRLF
+//     // headers CRLF
+//     // message-body
+//     // let http_request: Vec<_> = buf_reader
+//     //         .lines()
+//     //         .map(|result| result.unwrap())
+//     //         .take_while(|line|!line.is_empty())
+//     //         .collect();
+//     // println!("Request: {:#?}", http_request);
+
+//     let request_line = buf_reader
+//         .lines()
+//         .next()
+//         .unwrap()
+//         .unwrap();
+
+//     let (status_line, filename) =
+//         if request_line == "GET / HTTP/1.1" {
+//             ("HTTP/1.1 200 OK", "hello.html")
+//         } else {
+//             ("HTTP/1.1 404 NOT FOUND", "404.html")
+//         };
+
+//     let contents = fs::read_to_string(filename).unwrap();
+//     let length = contents.len();
+
+//     /* http response message */
+//     // HTTP-Version Status-Code Reason-Phrase CRLF
+//     // headers CRLF
+//     // message-body
+//     let response = format!(
+//         "{status_line}\r\n\
+//         Content-Length: {length}\r\n\r\n\
+//         {contents}"
+//     );
+
+//     stream.write_all(response.as_bytes()).unwrap();
+// }
+
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
@@ -22,47 +70,30 @@ fn handle_connection(mut stream: TcpStream) {
     // Method Request-URI HTTP-Version CRLF
     // headers CRLF
     // message-body
-    // let http_request: Vec<_> = buf_reader
-    //         .lines()
-    //         .map(|result| result.unwrap())
-    //         .take_while(|line|!line.is_empty())
-    //         .collect();
-    // println!("Request: {:#?}", http_request);
 
-    let request_line = buf_reader
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        /* http response message */
-        // HTTP-Version Status-Code Reason-Phrase CRLF
-        // headers CRLF
-        // message-body
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
 
-        let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\r\n\
-            {contents}"
-        );
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+    /* http response message */
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+    let response = format!(
+        "{status_line}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+    );
 
-        let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\r\n\
-            {contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    stream.write_all(response.as_bytes()).unwrap();
 }
-
