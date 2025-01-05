760491c320aafa387e7bdce5d0ded88bca0b4d0c
Rakuram <rakuram.e96@gmail.com>
2025-01-05 23:14:21 +0530

    add lists

---
+++ projects/lists/README.md
@@ -0,0 +1,3 @@
+This project is basically the practice of **Learn Rust With Entirely Too Many Linked Lists**. 
+
+**Link:** [Learn Rust With Entirely Too Many Linked Lists](https://rust-unofficial.github.io/too-many-lists/index.html)
---
+++ projects/lists/src/first.rs
@@ -0,0 +1,40 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+struct Node {
+    elem: i32,
+    next: List,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty ),
+        });
+        
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)  
+            }    
+        }
+    }
+}
